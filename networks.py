a73a82abf641cff3139f5d4c6596191b21c1ef75
eteh@uoguelph.ca <eteh@GPU9.uoguelph.ca>
2020-01-08 19:58:45 -0500

    update netwok code

--- networks.py
+++ networks.py
@@ -7,12 +7,12 @@ import xception
 class Model_bach(nn.Module):
     def __init__(self, nb_classes=4):
         torch.nn.Module.__init__(self)
-        base = torchvision.models.resnet34()
+        base = torchvision.models.resnet34(pretrained=True)
         #base = torchvision.models.resnet101(pretrained=True)
         #base = xception.xception(pretrained='imagenet')
        
-        #self.base = nn.Sequential(*list(base.children())[:-1])
-        self.base = base
+        self.base = nn.Sequential(*list(base.children())[:-1])
+        #self.base = base
         self.emb = nn.Linear(512, nb_classes)
         #self.emb = nn.Linear(2048, nb_classes)
 
